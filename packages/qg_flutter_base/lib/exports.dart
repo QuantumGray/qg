2b7e67d3da3b4424859883d8fab7a6b07a2e134d
Niklas von Haxthausen <nik.v.hax@gmail.com>
2022-06-06 11:00:57 +0200

    feat: added exports file

---
+++ packages/qg_flutter_base/lib/exports.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_hooks/flutter_hooks.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:riverbloc/riverbloc.dart';
+export 'package:bloc/bloc.dart';
