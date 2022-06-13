37510997b7e9f8bcb7615694d9a487a720e819b7
Niklas von Haxthausen <nik.v.hax@gmail.com>
2022-06-13 15:57:46 +0200

    misc

--- packages/qg_flutter_base/lib/exports.dart
+++ packages/qg_flutter_base/lib/exports.dart
@@ -3,3 +3,68 @@ export 'package:hooks_riverpod/hooks_riverpod.dart';
 export 'package:riverbloc/riverbloc.dart';
 export 'package:bloc/bloc.dart';
 export 'package:go_router/go_router.dart';
+// BLOCS
+export 'package:animated_page_reveal/animated_page_reveal.dart';
+export 'package:animated_text_kit/animated_text_kit.dart';
+export 'package:animated_tree_view/animated_tree_view.dart' hide StringUtils;
+export 'package:animations/animations.dart';
+export 'package:auto_animated/auto_animated.dart';
+export 'package:badges/badges.dart';
+export 'package:bloc/bloc.dart';
+// ARCHITECTURE
+export 'package:bloc_bus/bloc_bus.dart';
+//!export 'package:app_feedback/app_feedback.dart';
+export 'package:circular_clip_route/circular_clip_route.dart';
+export 'package:coachmaker/coachmaker.dart';
+export 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
+export 'package:expandable_sliver_list/expandable_sliver_list.dart';
+export 'package:feedback/feedback.dart';
+export 'package:flash/flash.dart';
+export 'package:flex_color_picker/flex_color_picker.dart';
+export 'package:flicked_cards/flicked_cards.dart';
+export 'package:flutter_advanced_avatar/flutter_advanced_avatar.dart';
+export 'package:flutter_chat_types/flutter_chat_types.dart'
+    hide FileMessage, ImageMessage, Message, TextMessage;
+export 'package:flutter_chat_ui/flutter_chat_ui.dart';
+export 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+export 'package:flutter_portal/flutter_portal.dart';
+//   # - WIDGETS
+export 'package:flutter_slidable/flutter_slidable.dart';
+export 'package:funvas/funvas.dart';
+export 'package:getwidget/getwidget.dart'
+    hide LinearPainter, CirclePainter, QueryBuilder;
+//!export 'package:scribble/scribble.dart';
+export 'package:glass_kit/glass_kit.dart';
+export 'package:glopos/glopos.dart';
+export 'package:go_router/go_router.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:lottie/lottie.dart';
+export 'package:macos_ui/macos_ui.dart';
+export 'package:motion_toast/motion_toast.dart';
+export 'package:parallax_animation/parallax_animation.dart';
+export 'package:percent_indicator/percent_indicator.dart';
+export 'package:pinput/pinput.dart';
+export 'package:pixel_perfect/pixel_perfect.dart';
+export 'package:plural_selectable/plural_selectable.dart' hide Selectable;
+export 'package:reorderables/reorderables.dart';
+export 'package:responsive_framework/responsive_framework.dart';
+//   # - ANIMATIONS
+export 'package:rive/rive.dart';
+export 'package:shimmer/shimmer.dart';
+export 'package:simple_animations/simple_animations.dart';
+export 'package:sliding_number/sliding_number.dart';
+export 'package:sliding_number/sliding_number.dart';
+export 'package:sliver_tools/sliver_tools.dart';
+export 'package:slyverin/slyverin.dart';
+export 'package:smooth_page_indicator/smooth_page_indicator.dart';
+export 'package:stacked_notification_cards/stacked_notification_cards.dart';
+// # - EXTENSIONS
+export 'package:styled_widget/styled_widget.dart';
+// export 'package:fluttertoast/fluttertoast.dart'; conflicts with flash
+export 'package:visibility_detector/visibility_detector.dart';
+export 'package:widget_mask/widget_mask.dart';
+
+export 'blocs/blocs.dart';
+// WIDGETS
+export 'widgets/widgets.dart';
